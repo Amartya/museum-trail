15fd3fc82b09ad66a2d9e0a551dc66d6fde17a9d
Amartya Banerjee <Amartya@dhcp-129-105-107-137.sesp.northwestern.edu>
2016-02-23 19:22:43 -0600

    location mapping now fully enabled

--- FieldRecorder/FieldRecorder/TrailViewController.swift
+++ FieldRecorder/FieldRecorder/TrailViewController.swift
@@ -8,48 +8,109 @@
 import Foundation
 import UIKit
 
-// 1. Add the EILIndoorLocationManagerDelegate protocol
+//add the EILIndoorLocationManagerDelegate protocol to hook into the Estimote SDK
 class TrailViewController: UIViewController, EILIndoorLocationManagerDelegate  {
+
+    @IBOutlet var indoorLocationOutput: UILabel!
     
-    // 2. Add the location manager
+    //add the location manager
     let locationManager = EILIndoorLocationManager()
     
+    //location being tracked
     var location: EILLocation!
     
+    //view used to visualize position from Cartesian Estimote coords to iOS coods
+    @IBOutlet weak var locationView: EILIndoorLocationView!
+    
+    @IBOutlet weak var traceSwitch: UISwitch!
+    
+    //toggle trace display
+    @IBAction func showTrace(sender: UISwitch) {
+        if let _ = self.locationView{
+            self.locationView.showTrace = sender.on
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 3. Set the location manager's delegate
-        self.locationManager.delegate = self
         
+        traceSwitch.enabled = true
+        
+        //the app id and token are generated in the Estimote cloud
         ESTConfig.setupAppID("museum-trail-oo1", andAppToken: "f6b8110fd473519734dfd2c48cf485e9")
         
-        var location = EILLocationBuilder.init()
-        location.setLocationBoundaryPoints([EILPoint(x: 0, y: 0),EILPoint(x: 0, y: 5),EILPoint(x: 5, y: 5),
-            EILPoint(x: 5, y: 0)])
+        //set the location manager's delegate
+        self.locationManager.delegate = self
         
-        location.setLocationOrientation(0)
         
-        let fetchLocationRequest = EILRequestFetchLocation(locationIdentifier: "my-kitchen")
+        /**
+        Uncomment this block to get a list of all saved locations
+        let fetchLocations = EILRequestFetchLocations()
+        fetchLocations.sendRequestWithCompletion{(locations, error) in
+            if let locations = locations{
+                print(locations)
+            }
+        }*/
+        
+        //try to fetch location from Estimote Cloud, otherwise, build location
+        //N.B. the locationIdentifier is different from the name we give to the location using setLocationName
+        let fetchLocationRequest = EILRequestFetchLocation(locationIdentifier: "annenberg-hall-commons-koi")
         fetchLocationRequest.sendRequestWithCompletion { (location, error) in
-            if location != nil {
-                self.location = location!
+            if let location = location {
+                self.location = location
+                self.completeLocationViewerSetup()
+            }
+            else {
+                let locationBuilder = EILLocationBuilder.init()
+                locationBuilder.setLocationBoundaryPoints([EILPoint(x: 0, y: 0), EILPoint(x: 0, y: 6.2), EILPoint(x: 9.78, y: 6.2), EILPoint(x: 9.78, y: 0)])
                 
-                self.locationManager.startPositionUpdatesForLocation(self.location)
-            } else {
-                print("can't fetch location: \(error)")
+                /**
+                Current list of the 6 beacons we have
+                    1 - F0:34:B5:DC:9C:CE
+                    2 - CA:9F:BB:01:B1:0A
+                    3 - EA:1F:87:62:C6:B8
+                    4 - D7:10:BB:1D:D9:18
+                    5 - C0:DC:98:37:75:9D
+                    6 - C3:15:A9:E0:2B:F3
+                */
+                locationBuilder.addBeaconWithIdentifier("F0:34:B5:DC:9C:CE", atBoundarySegmentIndex: 0, inDistance: 3.1, fromSide: EILLocationBuilderSide.LeftSide)
+                locationBuilder.addBeaconWithIdentifier("CA:9F:BB:01:B1:0A", atBoundarySegmentIndex: 1, inDistance: 4.89, fromSide: EILLocationBuilderSide.LeftSide)
+                locationBuilder.addBeaconWithIdentifier("EA:1F:87:62:C6:B8", atBoundarySegmentIndex: 2, inDistance: 3.1, fromSide: EILLocationBuilderSide.RightSide)
+                locationBuilder.addBeaconWithIdentifier("D7:10:BB:1D:D9:18", atBoundarySegmentIndex: 3, inDistance: 4.89, fromSide: EILLocationBuilderSide.RightSide)
+                
+                locationBuilder.setLocationOrientation(63.0)
+                locationBuilder.setLocationName("Annenberg Hall Commons")
+                
+                self.location = locationBuilder.build()
+                
+                //after building the location, save it to Estimote Cloud
+                let requestAddLocation = EILRequestAddLocation.init(location: self.location)
+                requestAddLocation.sendRequestWithCompletion({(EILRequestAddLocationBlock) in print("saved location")})
+                
+                self.completeLocationViewerSetup()
             }
         }
     }
     
-    func indoorLocationManager(manager: EILIndoorLocationManager!,
-        didFailToUpdatePositionWithError error: NSError!) {
+    func completeLocationViewerSetup(){
+        //configure the location view
+        self.locationView.showTrace = traceSwitch.on
+        self.locationView.rotateOnPositionUpdate = false
+        self.locationView.drawLocation(self.location)
+        
+        //setup up handler for position updates
+        self.locationManager.startPositionUpdatesForLocation(self.location)
+    }
+    
+    func indoorLocationManager(manager: EILIndoorLocationManager,
+        didFailToUpdatePositionWithError error: NSError) {
             print("failed to update position: \(error)")
     }
     
-    func indoorLocationManager(manager: EILIndoorLocationManager!,
-        didUpdatePosition position: EILOrientedPoint!,
+    func indoorLocationManager(manager: EILIndoorLocationManager,
+        didUpdatePosition position: EILOrientedPoint,
         withAccuracy positionAccuracy: EILPositionAccuracy,
-        inLocation location: EILLocation!) {
+        inLocation location: EILLocation) {
             var accuracy: String!
             switch positionAccuracy {
             case .VeryHigh: accuracy = "+/- 1.00m"
@@ -60,8 +121,11 @@ class TrailViewController: UIViewController, EILIndoorLocationManagerDelegate  {
             default: print("positioning not working")
             }
             
-            print(String(format: "x: %5.2f, y: %5.2f, orientation: %3.0f, accuracy: %@",
-                position.x, position.y, position.orientation, accuracy))
+            indoorLocationOutput.text = String(format: "x: %5.2f, y: %5.2f, orientation: %3.0f, accuracy: %@",
+                position.x, position.y, position.orientation, accuracy)
+            
+            //update position in the location view setup earlier
+            self.locationView.updatePosition(position)    
     }
     
     //hides the status bar
