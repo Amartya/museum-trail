a6aa93a4a5d7d6a85d76088747800ee1b55d0066
Amartya <amartya916@gmail.com>
2016-04-26 12:55:07 -0500

    for static files

--- digitalrail/digitalrail/urls.py
+++ digitalrail/digitalrail/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from  digitalrail.views import landingpage
 
 urlpatterns = [
     url(r'^$', landingpage.index, name='index'),
     url(r'^admin/', admin.site.urls),
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
