ffd4304f2a0e1712042035c3ae47cdac769d27ab
Amartya <amartya916@gmail.com>
2016-05-16 18:31:45 -0500

    major edit: started with reading json for new layout
    - also added in django admin for sqllite

--- digitalrail/digitalrail/views/landingpage.py
+++ digitalrail/digitalrail/views/landingpage.py
@@ -1,11 +1,23 @@
+import json
+
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.template import loader
+
+
+from digitalrail.models import Question
 
 def index(request):
     #return HttpResponse("Hello Digital Rail")
-    return render_to_response('digitalrail/landingpage/index.html',RequestContext(request))
+    return render_to_response('digitalrail/landingpage/index.html',context_instance=RequestContext(request))
+
 
 def bigquestion(request):
-    #return HttpResponse("Hello Digital Rail")
-    return render_to_response('digitalrail/attractscreen/bigquestion.html',RequestContext(request))
+    first_question = Question.objects.filter(active=True).order_by('-pub_date')[0]
+    question_data = {}
+    question_data['question'] = first_question.question_text
+    question_data['additional_prompt'] = first_question.additional_prompt
+
+    return render(request, 'digitalrail/attractscreen/bigquestion.html', question_data)
